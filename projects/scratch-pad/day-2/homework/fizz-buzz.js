3e3b3fedfe517aebfb0652a313caed61e86690e7
dREWbxKewb <drewvasqu1432@gmail.com>
2022-09-28 06:10:00 +0000

    stringy.js

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -12,6 +12,22 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
+    //Create a for loop that equals 0-100
+    for (var i = 1; i < 101; i++){
+      //create if/else if statement that takes into account a multiple of 15
+      if (i % 15 == 0){
+        //Log message
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0){ //else if multiple by 3
+        //Log message
+        console.log("Fizz");
+    } else if (i % 5 == 0){ //else if multiple of 5
+        //Log message
+        console.log("Buzz");
+    } else {//Else Log i
+        console.log(i);
+    }
+    } 
 
 
 
