b4a6303d407c0b65ad5939857e8be04a2bbdebef
dREWbxKewb <drewvasqu1432@gmail.com>
2022-09-30 05:57:42 +0000

    stringy.js

--- projects/scratch-pad/day-4/contact-list.js
+++ projects/scratch-pad/day-4/contact-list.js
@@ -35,7 +35,7 @@
 
 // YOUR CODE GOES BELOW HERE //
 function makeContact(id, nameFirst, nameLast) {
-    //Create a var obj as object literal
+    //1 //Create a var obj as object literal
     var obj = {};
     //Pass the parameter values to obj as keys
     obj.id = id;
@@ -50,15 +50,61 @@ function makeContactList() {
     /*
      * You need something here to hold contacts. See length api for a hint:
      */
-    var contacts = [];
+    var contacts = []; // Make contacts equal to array literal
     
     return {
         // we implemented the length api for you //
         length: function() {
             return contacts.length;
         },
+        // Create an addContact key that produces a function with the interger contact
         addContact: function(contact){
+            //Push contact interger into contacts array
             contacts.push(contact);
+        },
+        // Create a findContact function that produces a function with the interger fullName
+        findContact: function(fullName){
+            //Create a for loop that loops the contacts array for the object
+            for (var i = 0; i < contacts.length; i++){
+                // Create var that concatinating the nameFirst value and nameLast value in contacts array
+                var fName = contacts[i].nameFirst + " " + contacts[i].nameLast;
+                //Create a condition that takes var fName and strictly compares to fullName equally
+                if (fName === fullName){
+                    //return the object
+                    return contacts[i];
+                } 
+            }
+            //If loop does not find a match return undefined
+            return undefined;
+        },
+        //Create a function that removes the contact for array
+        removeContact: function(contact){
+            //Create a for loop that access the contact index
+            for (var i = 0; i < contacts.length; i++){
+                //Create a conditional statement that compares the contact index property values with each other
+                if(contacts[i].id === contact.id && contacts[i].nameFirst === contact.nameFirst && contacts[i].nameLast === contact.nameLast){
+                    //Remove the object from the array with splice
+                    contacts.splice(i, 1);
+                }
+            }
+        },
+        //Create function called printAllContactNames
+        printAllContactNames: function(){
+            //Create a var with string literal
+            var cString = "";
+            //Create a for loop that accesses the contacts array
+            for (var i = 0; i < contacts.length; i++){
+                //Create a conditional that checks if you have reached the last index
+                if(i === contacts.length - 1){
+                    //Add the concatinated nameFirst and nameLast values to cString
+                    cString += contacts[i].nameFirst + " " + contacts[i].nameLast
+                } else { // If not the end of the index
+                    //Add the concatinated values and add new line character
+                    cString += contacts[i].nameFirst + " " + contacts[i].nameLast + "\n";
+                }
+            }
+            //return var
+            return cString;
         }
     }
 }
